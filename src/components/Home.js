23429c7ba95c86951eec5b5b4abd74fc793e4435
Javier Torrez <javierctorrez@gmail.com>
2017-01-20 03:00:21 -0500

    Initial commit - Added basic routing

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {  
+  render() {
+    return (
+      <div className="main-content home">
+        <h2>Welcome Home!</h2>
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default Home;
